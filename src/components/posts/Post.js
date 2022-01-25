4a1b000d66ae3fb1d660433c7262c0de0448b173
luciidlou <lewisale96@gmail.com>
2022-01-25 15:00:46 -0600

    all of the posts are now rendered to the DOM

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,5 +1,24 @@
 import { useState } from "react"
 
-export const Post = () => {
+export const Post = (props) => {
     const [posts, setPosts] = useState([])
+
+    return (
+        <div className="card">
+            <header className="card-header">
+                <p className="card-header-title">
+                    {props.title}
+                </p>
+            </header>
+            <div className="card-content">
+                <div className="content">
+                    {props.content}
+                </div>
+            </div>
+            <footer className="card-footer">
+                <a href="#" className="card-footer-item">Edit</a>
+                <a href="#" className="card-footer-item">Delete</a>
+            </footer>
+        </div>
+    )
 }
--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react"
+import { PostsRepository } from "../../repositories/PostsRepository"
+import { Post } from "./Post"
+
 export const PostList = () => {
+    const [posts, setPosts] = useState([])
+    const [users, setUsers] = useState([])
+    const [categories, setCategories] = useState([])
+
+    const syncPosts = () => {
+        PostsRepository.getAll().then(setPosts)
+    }
+
+    useEffect(() => {
+        syncPosts()
+    }, [])
+
     return (
-        <h1>This is where we will iterate through all the posts and display them to the DOM</h1>
+        posts.map(post => {
+            // We still need to fetch users and categories from the 
+            const foundUser = users.find(user => user.id === post.userId)
+            const foundCategory = categories.find(category => category.id === post.categoryId)
+
+            return <Post
+            key={post.id}
+            title={post.title}
+            content={post.content}
+            publicationDate={post.publicationDate}
+            user={foundUser}
+            category={foundCategory}
+            />
+        })
     )
 }
