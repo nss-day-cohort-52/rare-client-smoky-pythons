1a2f9e91b4167d642a085b0118dd05b1473e79ff
luciidlou <lewisale96@gmail.com>
2022-01-26 10:36:42 -0600

    all posts and post details views are layed out

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -3,20 +3,16 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { PostForm} from "./posts/PostEntry"
 import { CategoryList } from "./categories/category_list"
+import { PostDetails } from "./posts/PostDetails"
+import { PostRoutes } from "./PostRoutes"
 
 export const ApplicationViews = () => {
   return (
     <>
-    <h1 >Welcome to Rare Publishing</h1>
-    <Route exact path = "/categories">
+    <Route exact path ="/categories">
       <CategoryList/>
     </Route>
-    <Route exact path="/posts">
-        <PostList />
-    </Route>
-    <Route exact path="/newPost">
-        <PostForm />
-    </Route>
+    <PostRoutes />
     </>
   )
 }
