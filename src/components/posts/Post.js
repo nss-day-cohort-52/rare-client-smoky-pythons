c257911807dcd413fe09e5e2e8e32dd2613737cb
luciidlou <lewisale96@gmail.com>
2022-01-26 10:50:02 -0600

    added styling when title of post is hovered over

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom"
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
+import "./Post.css"
 
 export const Post = (props) => {
     const history = useHistory()
     return (
         <tbody>
             <tr>
-                <td onClick={() => history.push(`/posts/${props.id}`)}>{props.title}</td>
+                <td id="titleLink" onClick={() => history.push(`/posts/${props.id}`)}>{props.title}</td>
                 <td>{props.user?.firstName} {props.user?.lastName}</td>
                 <td>{props.publicationDate}</td>
                 <td>{props.category}</td>
