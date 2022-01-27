4e265a3d2d98ddc9e71a5e615714d23691444734
charleslong13 <charleslong13@icloud.com>
2022-01-27 13:48:40 -0600

    csl-myposts

--- src/components/posts/MyPost.js
+++ src/components/posts/MyPost.js
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
+import {Post} from "./Post"
 
-
-export const MyPostList = () => {
-    const [posts, setPosts] = useState([])
+export const MyPostList = ({posts, syncPosts}) => {
+    const [post, setPosts] = useState([])
     const [myPosts, setMyPosts] = useState([])
     const [categories, setCategories] = useState([])
+    const [users, setUsers] = useState([])
     const history = useHistory()
 
     useEffect((
@@ -18,13 +19,31 @@ export const MyPostList = () => {
                 })
         }
     ), [])
+    useEffect((
+        () => {
+            fetch(`http://localhost:8088/users`)
+                .then(res => res.json())
+                .then((data) => {
+                    setUsers(data)
+                })
+        }
+    ), [])
+    useEffect((
+        () => {
+            fetch(`http://localhost:8088/categories`)
+                .then(res => res.json())
+                .then((data) => {
+                    setCategories(data)
+                })
+        }
+    ), [])
 
     const getMyPosts = () => {
         let myPosts = []
         const user = parseInt(localStorage.getItem("token"))
-        for (const post of posts) {
-            if (parseInt(post.user_id) === user) {
-                myPosts.push(post)
+        for (const myPost of post) {
+            if (parseInt(myPost.user_id) === user) {
+                myPosts.push(myPost)
             }
         }
         return myPosts
@@ -40,10 +59,10 @@ export const MyPostList = () => {
     
     return (
         <>
-            <table className="table">
                 <div>
                     <center> <Link to="/newPost" className="navbar-item">New Post</Link></center>
                 </div>
+            <table className="table">
                 <thead>
                     <tr>
                         <th>Title</th>
@@ -55,18 +74,26 @@ export const MyPostList = () => {
                 </thead>
                 {
                     getMyPosts().map(post => {
+                        const foundUser = users.find(user => user.id === post.userId)
                         const foundCategory = categories.find(category => category.id === post.categoryId)
-                        return <div key={post.id} className='my_post'>
-                            <div>{post.title}</div>
-                            <div>{post.publication_date}</div>
-                            <div>{post.content}</div>
-                            <div>{post.user_id?.name}</div>
-                            <div>{foundCategory}</div>
-
-
-                            <button onClick={() => { if (confirm('Are you sure you want to delete this post?') == true) delete_post(post.id) }}>Delete</button></div>
-                    })
-                }
+                        
+                        return (
+                            <>
+                            <Post
+                                key={post.id} 
+                                postId={post.id} 
+                                title={post.title}
+                                content={post.content}
+                                publicationDate={post.publication_date}
+                                user={foundUser}
+                                category={foundCategory}
+                                syncPosts={syncPosts}
+                                />
+                     {/* <button>Edit</button>
+                     <button onClick={() => { if (confirm('Are you sure you want to delete this post?') == true) delete_post(post.id) }}>Delete</button></div> */}
+                            </>
+                     ) })
+                    }
             </table>
         </>
     )
