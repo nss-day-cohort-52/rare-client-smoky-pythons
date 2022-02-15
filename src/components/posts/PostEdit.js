8e656adf406c0974b0bd9150e250c84b7ce84b8c
luciidlou <lewisale96@gmail.com>
2022-02-15 15:38:47 -0600

    users can now create new posts and edit existing ones

--- src/components/posts/PostEdit.js
+++ src/components/posts/PostEdit.js
@@ -1,54 +1,60 @@
 import { useEffect, useState } from "react"
 import { getCategories } from "../../repositories/CategoriesRepository"
 import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min"
-import { getSinglePost } from "../../repositories/PostsRepository"
+import { getSinglePost, PostsRepository } from "../../repositories/PostsRepository"
 import { getTags } from "../../repositories/TagsRepository"
 
 
 export const EditPost = ({ posts, syncPosts }) => {
 
+    const history = useHistory()
     const [categories, setCategories] = useState([])
     const [tags, setTags] = useState([])
     const { postId } = useParams()
-    const [post, setPost] = useState({})
-    const [newPost, setNewPost] = useState({
-        user_id: post.user_id,
-        category_id: post.category_id,
-        title: post.title,
-        publication_date: post.publication_date,
-        content: post.content,
+    const [post, updatePost] = useState({
+        category: 0,
+        title: "",
+        image_url: "",
+        content: "",
         tags: []
     })
-    const history = useHistory()
-
-    const updatePost = (id, updatedPost) => {
-        return fetch(`http://localhost:8000/posts/${id}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(updatedPost)
-        })
-    }
-
-    useEffect(() => {
-        getSinglePost(postId).then(setPost)
-    }, [postId])
-
-    const save_update = () => {
-        const updatedPost = Object.assign({}, post)
-        updatePost(postId, updatedPost)
-            .then(() => { syncPosts() })
-            .then(() => history.push(`/posts/${postId}`))
-
-    }
-
 
     useEffect(() => {
         getTags().then(setTags)
         getCategories().then(setCategories)
-    }, [])
+        getSinglePost(postId).then(post => {
+            updatePost({
+                category: post.category,
+                title: post.title,
+                image_url: post.image_url,
+                content: post.content,
+                tags: post.tags
+            })
+        })
+    }, [postId])
 
+    const SubmitEdit = (event) => {
+        event.preventDefault()
+
+        PostsRepository.update(postId, post)
+            .then(syncPosts)
+            .then(() => history.push(`/posts`))
+
+    }
+
+
+    const checkboxOnChange = () => {
+        const checkboxes = document.querySelectorAll('input[type=checkbox]')
+        const copy = { ...post }
+        let tagsArr = []
+        for (const box of checkboxes) {
+            if (box.checked) {
+                tagsArr.push(parseInt(box.value))
+            }
+        }
+        copy.tags = tagsArr
+        updatePost(copy)
+    }
 
     const generateCheckboxes = () => {
         const checkboxes = document.querySelectorAll('input[type=checkbox]')
@@ -67,7 +73,7 @@ export const EditPost = ({ posts, syncPosts }) => {
                         tags.map((tag) => {
                             return <div key={tag.id} className="option">
                                 <input className="checkbox" type="checkbox" id={tag.id} name="tags" value={tag.id}
-                                    onChange={setPost}>
+                                    onChange={checkboxOnChange}>
                                 </input>
                                 <label className="checkbox-label" htmlFor={tag.id}>{tag.label}</label>
                             </div>
@@ -78,55 +84,94 @@ export const EditPost = ({ posts, syncPosts }) => {
         )
     }
 
-    // const displayCheckboxes = generateCheckboxes()
+    const displayCheckboxes = generateCheckboxes()
 
     return (
-        <div className="edit-form">
-            <b>Edit Post</b>
-            <div className="field">Title:</div>
-            <input id="title" value={post.title} onChange={
-                (evt) => {
-                    //creates a copy of post state
-                    const copy = { ...post }
-                    copy.title = evt.target.value
-                    setPost(copy)
-                }
-            } required autoFocus
-                type="text"
-                className="form-control" >
-            </input>
+        <form className="requestForm">
 
-            <div className="field">Content:</div>
-            <textarea id="edit__content" value={post.content} onChange={
-                (evt) => {
-                    const copy = { ...post }
-                    copy.content = evt.target.value
-                    setPost(copy)
-                }
-            } required autoFocus
-                type="text"
-                className="form-control" >
-            </textarea>
-            <div className="field">
-                <div className="select">
-                    <select className="select" name="category_id" id="category_id" value={post.category_id} onChange={(evt) => {
-                        const copy = { ...post }
-                        copy.category_id = evt.target.value
-                        setPost(copy)
-                    }}>
-                        {
-                            categories.map((category) => {
-                                return <option key={category.id} value={category.id}>{category.label}</option>
-                            })
-                        }
-                    </select>
+            <h2 className="requestForm__title">New Post</h2>
+
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="postTitle">Title:</label>
+                    <input
+                        className="input"
+                        type="text"
+                        placeholder="Enter your title here"
+                        value={post.title}
+                        onChange={
+                            (evt) => {
+                                const copy = { ...post }
+                                copy.title = evt.target.value
+                                updatePost(copy)
+                            }
+                        } />
                 </div>
-            </div>
-            {/* {displayCheckboxes} */}
-            <div className="field">
-                <button className="save-button" onClick={save_update}>Save</button>
-            </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="content">Content:</label>
+                    <textarea
+                        className="textarea"
+                        placeholder="Enter Content Here"
+                        value={post.content}
+                        onChange={
+                            (evt) => {
+                                const copy = { ...post }
+                                copy.content = evt.target.value
+                                updatePost(copy)
+                            }}>
+                    </textarea>
 
-        </div >
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="image_url">Image URL:</label>
+                    <input
+                        className="input"
+                        placeholder="Enter Image URL here"
+                        value={post.image_url}
+                        onChange={
+                            (evt) => {
+                                const copy = { ...post }
+                                copy.image_url = evt.target.value
+                                updatePost(copy)
+                            }} />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <div className="select">
+                        <select name="category"
+                            proptype="int"
+                            value={post.category}
+                            onChange={
+                                (evt) => {
+                                    const copy = { ...post }
+                                    copy.category = parseInt(evt.target.value)
+                                    updatePost(copy)
+                                }
+                            }>
+
+                            <option value="0">Select a category</option>
+                            {categories.map(c => (
+                                <option key={c.id} value={post.category}>
+                                    {c.label}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    {displayCheckboxes}
+                </div>
+            </fieldset>
+            <button onClick={SubmitEdit} className="btn btn-primary" >
+                Submit Post!
+            </button>
+        </form>
     )
 }
